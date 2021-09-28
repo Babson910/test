0d307c2b7aa752b0eb5ec054433333e4a1bdc6c9
babs <babaly.diop91@gmail.com>
2021-09-28 11:49:08 +0200

    1er commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[]args) {
+		 Scanner clavier = new Scanner(System.in);
+	
+		 int nb1 = 10;
+		 int nb2 = 20;
+		 
+		 System.out.println(nb1 + nb2);
+}
+}
